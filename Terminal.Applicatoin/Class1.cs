556bc38511b051c1f9c49d03bfe11e6109ff633e
Pavel Borovkov(PC) <pavel3458@mail.ru>
2024-04-27 03:17:51 +0500

    готовим к релизу

--- Terminal.Applicatoin/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Terminal.Applicatoin
-{
-    public class Class1
-    {
-
-    }
-}
--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -10,76 +10,76 @@
 
 
 
-const select = document.querySelector('select');
+    let select = document.querySelector('select');
 
-select.onchange = function () {
-    let ControllerId = $("#commandId").val();
-    $.ajax({
-        type: 'GET',
-        url: '/Home/ParametersPartialView',
-        data: { "ControllerId": ControllerId },
-        success: function (response) {
-            $("#parameters").html($(response))
-        },
-        failure: function () {
-            alert("failure");
-            modal.modal('hide')
-        },
-        error: function (response) {
-            alert("ошибка");
+    select.onchange = function () {
+        let ControllerId = $("#commandId").val();
+        $.ajax({
+            type: 'GET',
+            url: '/Home/ParametersPartialView',
+            data: { "ControllerId": ControllerId },
+            success: function (response) {
+                $("#parameters").html($(response))
+            },
+            failure: function () {
+                alert("failure");
+                modal.modal('hide')
+            },
+            error: function (response) {
+                alert("ошибка");
+            }
+        });
+    }
+
+    let a = 1;
+    function myFunction() {
+        let terminalId = document.getElementById('id').value
+        let p1, p2, p3
+        if (document.getElementById('parameter_value1') == null) {
+            p1 = 0;
         }
-    });
-}
+        else
+            p1 = document.getElementById('parameter_value1').value;
 
-var a = 1;
-function myFunction() {
-    let terminalId = document.getElementById('id').value
-    let p1, p2, p3
-    if (document.getElementById('parameter_value1') == null) {
-        p1 = 0;
-    }
-    else
-        p1 = document.getElementById('parameter_value1').value;
-
-    if (document.getElementById('parameter_value2') == null) {
-        p2 = 0;
-    }
-    else
-        p2 = document.getElementById('parameter_value2').value;
-    if (document.getElementById('parameter_value3') == null) {
-        p3 = 0;
-    }
-    else
-        p3 = document.getElementById('parameter_value3').value;
-    var myDictionary = {
-        command_id: $("#commandId").val(),
-        parameter1: p1,
-        parameter2: p2,
-        parameter3: p3
-    };
-
-    $.ajax({
-        type: 'POST',
-        url: '/Home/TerminalResponse',
-        data: {
-            "GetParams": myDictionary,
-            "terminalId": terminalId
-        },
-        success: function (response) {
-            $("#History").append("<tr><th>" + a + "</th >" + response + "</tr>");
-            a++;
-        },
-        failure: function () {
-            alert("failure");
-            modal.modal('hide')
-        },
-        error: function (response) {
-            console.error(xhr.responseText);
-            alert("ошибка");
+        if (document.getElementById('parameter_value2') == null) {
+            p2 = 0;
         }
-    });
-    
-}
+        else
+            p2 = document.getElementById('parameter_value2').value;
+        if (document.getElementById('parameter_value3') == null) {
+            p3 = 0;
+        }
+        else
+            p3 = document.getElementById('parameter_value3').value;
+        var myDictionary = {
+            command_id: $("#commandId").val(),
+            parameter1: p1,
+            parameter2: p2,
+            parameter3: p3
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/Home/TerminalResponse',
+            data: {
+                "GetParams": myDictionary,
+                "terminalId": terminalId
+            },
+            success: function (response) {
+                $("#History").append("<tr><th>" + a + "</th >" + response + "</tr>");
+                a++;
+            },
+            failure: function () {
+                alert("failure");
+                modal.modal('hide')
+            },
+            error: function (response) {
+                console.error(xhr.responseText);
+                alert("ошибка");
+            }
+        });
+
+    }
 
 
 
